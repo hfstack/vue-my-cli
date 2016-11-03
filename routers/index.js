4ecab1fed600af37a8ae45c7915cdd68cfe3ad2d
huanglongfei <huanglongfei@duiba.com.cn>
2016-11-03 20:11:06 +0800

    first commit

---
+++ routers/index.js
@@ -0,0 +1,7 @@
+exports.admin = function (req, res) {
+  res.redirect('/src/views/admin/admin.html')
+}
+exports.header = function (req, res) {
+  res.redirect('/src/views/header/header.html')
+}
+
---
+++ src/views/header/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+// window.Vue = Vue
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: function (resolve) {
+        require(['./pages/Hello.vue'], resolve)
+      }
+    }
+
+  ]
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app-container')
